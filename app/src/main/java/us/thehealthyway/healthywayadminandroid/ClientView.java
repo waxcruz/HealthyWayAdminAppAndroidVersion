68f11c74c3a008ca966bbb3a3d064b999fb6aaef
Bill Weatherwax <waxcruz@yahoo.com>
2019-04-12 09:57:27 -0700

    Added permissions flow for Storage access

--- app/src/main/java/us/thehealthyway/healthywayadminandroid/ClientView.java
+++ app/src/main/java/us/thehealthyway/healthywayadminandroid/ClientView.java
@@ -1,13 +1,16 @@
 package us.thehealthyway.healthywayadminandroid;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.renderscript.Allocation;
 import android.security.keystore.KeyNotYetValidException;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -230,7 +233,10 @@ private class EditTextListener implements TextWatcher {
             Log.d(TAG, "sendEmailToStaff: send client journal to staff");
         }
         // check permissions for attachments
-
+        if (!clearPermissionsForStorageAccess()) {
+            message.setText("Try again after you permit access to storage");
+            return;
+        };
         // build journal
         String htmlBodyMail = formatJournal(model.getClientNode(), true);
         // save html to local file
@@ -782,5 +788,66 @@ private class EditTextListener implements TextWatcher {
         }
         return mmDD;
     }
+
+
+    // permissions handling
+    private boolean clearPermissionsForStorageAccess(){
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        HealthyWayAdminActivities.PERMISSION_READ_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return false;
+        } else {
+            Log.i(TAG, "clearPermissionsForStorageAccess: permission granted");
+            return true;
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case HealthyWayAdminActivities.PERMISSION_READ_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "onRequestPermissionsResult: permission granted for read");
+                } else {
+                    message.setText("You must permit Storage or journal email will fail");
+                }
+                return;
+            }
+            case HealthyWayAdminActivities.PERMISSION_WRITE_STORAGE: {
+                if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "onRequestPermissionsResult: permission granted for write");
+                } else {
+                    message.setText("You must permit Storage or journal email will fail");
+                }
+            }
+        }
+    }
+
 }
 
